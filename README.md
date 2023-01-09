159e07a931e462fd60b5a32e78b30a437b2bcd01
Paul Rentschler <paul@rentschler.ws>
2023-01-08 19:02:03 -0500

    Package setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dj-bootstrap-forms
+# DJ Bootstrap Forms
+
+Render Django forms for use with Bootstrap styles
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '22.12.1'
+
+setup(
+    name='DJ Bootstrap Forms',
+    version=version,
+    description='Render Django forms for use with Bootstrap styles',
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Programming Language :: Python',
+    ],
+    keywords='Django Forms Bootstrap',
+    author='Paul Rentschler',
+    author_email='paul@rentschler.ws',
+    url='https://github.com/paulrentschler/dj-bootstrap-forms.git',
+    license='MIT License',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
